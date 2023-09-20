f291efaec669cb4f1e4b7b4b812e51559f8e168d
merge <fpel8800@gmail.com>
2023-09-20 21:29:38 +0900

    update: global style 적용 및 레이아웃 구성 작업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { Navigation } from "../layouts";
+import "./global.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <Navigation />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
